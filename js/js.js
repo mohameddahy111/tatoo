a2e52fbae49faa29e53cc3a0c12de9fd9d9bab66
mohameddahy dahy <mohameddahy111@gmail.com>
2023-05-05 18:58:41 +0300

    last updata 5/5/2023

--- js/js.js
+++ js/js.js
@@ -1,8 +1,20 @@
-$('#btnMenu').click(()=>{
-    $('.mobil-menu').show(500);
+let token = "IGQVJYUFVSVE11WUpCdGp4QkcwS0ZA4WVVoenhtOHJyYTRhTExwYnFNS3dGV1RDU3BwRklKaW5feDQ3MHU4V2RLcDd6dlZAjYmRwbVZAPT3RCZAkxLTjFyYy1jX283eWh6ZAkd6SXk0RFBCUXNaSmRhNmlvMwZDZD"
+
+
+$('#btnMenu').click(() => {
+  $('.mobil-menu').show(500);
+});
+$('#closeBtn').click(() => {
+  $('.mobil-menu').hide(500);
+});
+var userFeed = new Instafeed({
+  get: 'user',
+  target: "instafeed",
+    resolution: 'low_resolution',
+  accessToken: token,
+  limit:6,
+  template:`<div class="col-12 col-md-4 p-2 " > <a href="{{link}}"><img title="{{caption}}" src="{{image}}" /></a></div>`
+
   
-  })
-  $('#closeBtn').click(()=>{
-    $('.mobil-menu').hide(500);
-  })
-// ea1754b32e19cf01abc586426b20d62f
+});
+userFeed.run();
